ff1e6f14fff71b22c44055e787f453c1dd7ee634
ankit <ankitkr5858@gmail.com>
2019-03-23 17:29:33 +0530

    Login page initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+ReactiveFormsModule,
+FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormControl, FormGroup,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+signUp= new FormGroup({
+  username: new FormControl('', Validators.required),
+  password: new FormControl('',[Validators.required,Validators.minLength(6)])
+})
+ 
+onSignup(){
+
+}
+get username(){
+  return this.signUp.get('username');
+}
+get password(){
+  return this.signUp.get('password');
+}
+
+
 
 }
